4b9eb747c977b41ba312268d26dfd717019477ea
Sukera <Seelengrab@users.noreply.github.com>
2020-10-01 12:30:04 +0200

    Bump version to 0.2.0, added facilities for easier property writing, added shrinking of test cases

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A simple, thin package for property based testing. This package, though function
 
 ## Installation
 
-This package is not yet registered with METADATA, so to install do
+This package is not yet registered with General, so to install do
 
 ```julia
 pkg> add https://github.com/Seelengrab/PropCheck.jl.git
@@ -12,7 +12,7 @@ pkg> add https://github.com/Seelengrab/PropCheck.jl.git
 
 ## Usage
 
-First define a property you want to test, which for PropCheck.jl means defining a function which takes some arguments, checks that property against the arguments and returns a boolean indicating whether the property holds or not.
+First define a property you want to test, which for PropCheck.jl means defining a function, checks that property and returns a boolean indicating whether the property holds or not.
 
 For example for addition:
 
@@ -23,10 +23,12 @@ function add(x::Int, y::Int)
 end
 
 # The commutative property for addition:
-function commProp(x::Int, y::Int)
-    c = add(x,y)
-    d = add(y,x)
-    return c == d
+function commProp()
+    @forall((generate(Int),generate(Int)), (x,y) -> begin
+      c = add(x,y)
+      d = add(y,x)
+      return c == d
+    end)
 end
 ```
 
@@ -35,44 +37,40 @@ In order to test this property, the `@check` macro is provided:
 ```julia
 julia> using PropCheck
 
-julia> @check commProp (Int, Int)
+julia> @check commProp
 Test Summary: | Pass  Total
 commProp: ✓   |    1      1
 ```
 
-Note the tuple provided after the name of the property - it defines the argument types of the generated values to test the given function with.
-
 If you have more than one property to test, you can simply nest the `@check` calls into another testset from e.g. `Test`:
 
 ```julia
-julia> function identProp(x::Int)
-           x + 0 == 0 + x
+julia> function identProp()
+           @forall(generate(Int), x -> x + 0 == 0 + x)
        end
 
 julia> using Test
 
 julia> @testset "Nested Tests" begin
-           @check commProp (Int, Int)
-           @check identProp (Int,)
+           @check commProp
+           @check identProp
        end
 Test Summary: | Pass  Total
 Nested Tests  |    2      2
 ```
 
-For the `identProp` property, note the single argument. In order to convey this to PropCheck, pass a single value tuple.
-
 Failing properties will be displayed as well:
 
 ```julia
-julia> brokenProp(::Int, ::Int) = false
+julia> brokenProp() = @forall(generate(Int), _ -> false)
 brokenProp (generic function with 1 method)
 
 julia> @testset "A broken test" begin
-           @check commProp (Int, Int)
-           @check identProp (Int,)
-           @check brokenProp (Int, Int)
+           @check commProp
+           @check identProp
+           @check brokenProp
        end
-brokenProp: [0, 0]: Test Failed at [...]
+brokenProp: 152341: Test Failed at [...]
   Expression: res
 Stacktrace:
     [...]
@@ -80,21 +78,19 @@ Test Summary:        | Pass  Fail  Total
 A broken test        |    2     1      3
   commProp: ✓        |    1            1
   identProp: ✓       |    1            1
-  brokenProp: [0, 0] |          1      1
+  brokenProp: 152341 |          1      1
 ```
 
-Note the `[0,0]` indicating for which input the property doesn't hold. By default, some special values are generated for numeric types. Should a type not support generation, an error will be thrown indicating so. After the special types are tested, 100 random instances will be generated and tested. there is no "shrinking" of failed cases. Errors occuring during testing are considered a failure and are printed together with the case which produced the error. This does not stop checking of the remaining properties, but does prevent further checks of the same property.
+Note the `152341`, indicating for which input the property doesn't hold. By default, most basic types have a generic generator defined. Should a type not support generation, PropCheck will fall back to generating via reflection of the fields of the type. If the types doesn't support the default constructor, you will have to define a `PropCheck.generate` method for your type. Generating `Any` is supported, but be warned that this _will_ give you an instance of any known type that can be generated without errors as well as kill any hopes of working inference.
+
+Once a falsifying example has been found, PropCheck tries to shrink it to a minimal example. Arrays shrink towards the empty array, numbers shrink towards zero, tuples only shrink their elements. If you want to customise shrinking for your type, define `PropCheck.shrink` for your type.
+
+Errors occuring during testing are considered a failure and are printed together with the case which produced the error. This does not stop checking of the remaining properties, but does prevent further checks of the same property.
 
 ## ToDo
 
 In no particular order:
 
  * Clean up the printing of test cases (custom testset?)
- * Finish reflection of argument types on properties
-   * Make it type stable
- * Make special case generation type-stable
  * Define more generators for types from Base, e.g. `Char` and `String`
- * Better value generation
-   * Provide a way to exclude certain values and/or ranges of values
-   * Provide a way to pass "must-check" values or ranges of values
-   * Provide a way to set a custom number of randomly generated values
+ * Make it possible to `generate(Union{Int,Float64})`
--- src/PropCheck.jl
+++ src/PropCheck.jl
@@ -1,81 +1,298 @@
 module PropCheck
 
-using Test, Logging
+using Test
+using InteractiveUtils: subtypes
 
-export generate, specials, checkProp, argTypes, @check
+export generate, @check, @forall, @suchthat, @set, shrink
 
-getsig(f::Function) = map(b -> b.sig, methods(f).ms)
-function argTypes(f::Function)
-    filtered = filter(x -> !(x isa UnionAll) && !(Any in x.parameters), getsig(f))
-    return map(x -> x.parameters[2:end], filtered)
-end
+"""
+Total number of tries to generate an element using [`@suchthat`](@ref).
+"""
+const nGenTries = Ref(100)
 
-function lenReduce(x, y)
-    return x * length(y)
-end
+"""
+Total number of times a given usage of [`@forall`](@ref) tries to find a falsifying case.
+"""
+const nTests = Ref(100)
 
-function genSpecialCases(types::NTuple{N, DataType}) where {N}
-    specialCases = specials.(types)
-    ncases = foldl(lenReduce, specialCases, init=1)
-    cases = Matrix{Union{types...}}(undef, ncases, N)
-    for i in axes(cases, 2)
-        lenEach = (i == N) ? 1 : foldl(lenReduce, specialCases[i+1:end], init=1)
-        cases[:,i] .= repeat(specialCases[i], inner = lenEach, outer = div(ncases, lenEach*length(specialCases[i])))
-    end
-    cases
-end
+"""
+Total number of shrinks attempted during the shrinking.
+"""
+const nShrinks = Ref(1000)
 
-function checkProp(prop::Function, types::NTuple{N, DataType}, ntests = 100) where {N}
-    ret, caseSpec = checkSpecials(prop, types)
-    !ret && return false, caseSpec
-    ret, caseGen = checkGen(prop, types, ntests)
-    !ret && return false, caseGen
-    return true, nothing
-end
+"""
+Probability of dropping an element of an arraylike during shrinking.
+"""
+const dropChance = Ref(0.5)
 
-function checkSpecials(prop::Function, types::NTuple{N, DataType}) where {N}
-    specials = genSpecialCases(types)
-    for case in axes(specials, 1)
-        try
-            !prop(specials[case,:]...) && return false, specials[case,:]
-        catch ex
-            return false, (ex, specials[case,:])
+"""
+    @suchthat(N, generate(Int), x -> ...)
+    @suchthat(N, () -> generate(Int), ==(N))
+    @suchthat((N,M), (generate(Int),generate(Int)), !=)
+
+Tries to generate elements via the given generator(s) until the condition is satisfied. The condition has to return a boolean.
+"""
+macro suchthat(bin, gen, cond, tries=nGenTries[])
+    condArgs = cond.args[1] isa Symbol ? bin : :($bin...)
+    generator = gen isa Symbol || (gen isa Expr && gen.head == :->) ? :($gen()) :
+                (gen isa Expr && (gen.head == :call || gen.head == :tuple)) ? gen : :(() -> error("The given generator does not look like a callable or a function call."))
+
+    return esc(quote
+        $bin = $generator
+        for i in 1:$tries
+            i == $tries && error("Couldn't find a workable case in $i tries.")
+            
+            $cond($condArgs) && break
+            $bin = $generator
         end
-    end
-    return true, nothing
+        $bin
+    end)
 end
 
-function checkGen(prop::Function, types::NTuple{N, DataType}, ntests) where {N}
-    for _ in 1:ntests
-        case = generate.(types)
-        try
-            !prop(case...) && return false, case
-        catch ex
-            return false, (ex, case)
+"""
+    @set(N, generate(Int), N*2)
+
+Generates values from the given generators and applies the expression `expr` on them. Leaks definitions into the containing scope!
+"""
+macro set(bin, gen, expr)
+    # TODO: Find out if this macro is really necessary, seems like a trivial transform?
+    generator = gen isa Symbol || (gen isa Expr && gen.head == :->) ? :($gen()) : gen isa Expr && gen.head == :call ? gen : :(() -> error("The given generator does not look like a callable or a function call."))
+
+    esc(quote
+        $bin = $generator
+        $bin = $expr
+    end)
+end
+
+"""
+    @forall(generate(Int), x -> (x == x), nTests=PropCheck.nTests[])
+    @forall((generate(Int), generate(Int)), x -> (x == x), nTests=PropCheck.nTests[])
+    @forall((generate(Int), generate(Int)), (x,y) -> (x != y), nTests=PropCheck.nTests[])
+    @forall((generate(Int), generate(Int)), !=(x,y), nTests=PropCheck.nTests[])
+
+Generates testcases from the given generator `gen` and checks whether the property `prop` holds. Tries a maximum of `nTests` different testcases. If a failing testcase is found, i.e. `prop(case...)` returns `false` for some `case`, the case is shrunk to a minimal reproducible example. The number of arguments is tried to be inferred from the given property. 
+
+Returns either `(true, nothing)` or `(false, <case>)`.
+"""
+macro forall(gen, prop, nTests=nTests[])
+    condGen = gen isa Symbol || (gen isa Expr && gen.head == :->) ? :($gen()) :
+             (gen isa Expr && (gen.head == :call || gen.head == :tuple) ? gen : error("The given generator does not look like a generator."))
+
+    if !(prop isa Expr && (prop.head == :-> || prop.head == :call))
+        error("Can't infer number of arguments for function.")
+    end
+
+    if prop.head == :call
+        singleArg = length(prop.args) == 2
+        prop = prop.head == :call ? prop.args[1] : prop
+    elseif prop.head == :->
+        singleArg = prop.args[1] isa Symbol
+    end
+    condCase = singleArg ? :case : :(case...)
+    
+    return esc(quote
+        for i in 1:$nTests
+            case = $condGen
+            try 
+                if !$prop($condCase)
+                    println("\n\tFailed after $i test", i != 1 ? "s." : ".")
+                    print("\tShrinking... ")
+                    s = shrink($prop, case, $singleArg)
+                    case = s.case
+                    shrinks = s.generation
+                    println("(", shrinks, " time", shrinks != 1 ? "s)" : ")")
+                    println('\t', case, '\n')
+                    return false, case
+                end
+            catch ex
+                println("\tGot exception:")
+                println('\t', ex)
+                return false, case                
+            end
         end
-    end
-    return true, nothing
+        println("✓")
+        return true, nothing
+    end)
 end
 
-macro check(func::Symbol, types, x...)
-    quote
-        local escf = $(esc(func))
-        local res, case = checkProp($(esc(func)), $(esc(types)))
-        case = case === nothing ? "✓" : case
-        @testset "$(nameof(escf)): $case" begin
+"""
+    @check customProperty
+    @check customProperty()
+
+Evaluates the given property and tests whether or not it holds. The given function should not take any arguments.
+"""
+macro check(func)
+    f = func.head == :call ? f : :(f())
+
+    esc(quote
+    local f = "$($func)"
+    @testset "$($func)" begin
+            print(f, ": ")
+            local res, _ = $f
             @test res
         end
-    end
+    end)
 end
 
-generate(::Type{T}) where {T <: Number} = rand(T)
-generate(::Type{T}) where {T} = error("No `generate` defined for $T")
-generate(::Type{T}, n) where {T} = T[ generate(T) for _ in 1:n ]
+struct empty end
+# This kills inference and might result in endless recursion, so be careful with ::Any field type
+generate(::Type{Any}) = begin
+    x = empty()
+    while x === empty()
+        x = try
+            generate(rand(filter(isconcretetype, subtypes(Any))))
+        catch ex
+            # ignore
+            empty()
+        end
+    end
+    x
+end
+# FIXME: currently only dispatched on if no other method is more special, e.g. Union{Int,Float64} would be dispatched to the generator for <:Number...
+generate(x::Union) = generate(rand(filter(isconcretetype, Base.uniontypes(x))))
 
-specials(::Type{T}) where {T <: Signed} = T[zero(T), one(T), -one(T), typemax(T), typemin(T)]
-specials(::Type{BigInt}) = BigInt[zero(BigInt),one(BigInt),-one(BigInt)]
-specials(::Type{T}) where {T <: Unsigned} = T[zero(T), one(T), typemax(T)]
-specials(::Type{T}) where {T <: AbstractFloat} = T[zero(T), one(T), typemax(T), typemin(T), zero(T) / zero(T)]
-specials(::Type{T}) where {T} = T[generate(T)]
+"""
+    generate(::T)
+
+Fallback generator for any given struct. Iterates over all field types and generates an instance recursively. Fails horribly on errors occuring during construction, write a special generator if that's the case.
+
+If a given type doesn't have a special generator defined, this will break implicit invariants not enforced in the constructor! (but that's the point of all of this anyway)
+"""
+generate(::Type{T}) where T = T([ generate(f) for f in fieldtypes(T) ]...) # default fallback for structs
+
+"""
+    generate(::Type{T}) where { T <: Number}
+
+Generates a number by calling `rand(T)`.
+"""
+generate(::Type{T}) where {T <: Number} = rand(T) # numbers
+
+"""
+    generate(::Type{T}) where {V, N, T <: AbstractArray{V,N}}
+
+Generates arrays by choosing a random number of dimensions and filling the resulting array by repeatedly calling `generate(V)`.
+"""
+generate(::Type{T}) where {V, N, T <: AbstractArray{V,N}} = begin
+    res = T(undef, rand(UInt8, N)...)
+    for i in eachindex(res)
+        res[i] = generate(V)
+    end
+    res
+end
+
+generate(::Type{NTuple{N,T}}) where {N, T} = begin
+    data = collect(generate(T) for _ in 1:N)
+    ntuple(x -> data[x], N)
+end
+
+struct Shrinker{T}
+    case::T
+    generation::UInt
+end
+Shrinker(case) = Shrinker{typeof(case)}(case, UInt(0))
+
+Base.isless(a::T, b::T) where {S, T <: Shrinker{S}} = shrinkless(a.case,b.case)
+shrinkless(a::T, b::T) where T <: Number = a < b
+shrinkless(a::T, b::T) where T <: Tuple = length(a) == length(b) ? reduce(&, map(shrinkless, a, b)) : length(a) < length(b)
+shrinkless(a::T, b::T) where T = begin
+    try
+        return a < b
+    catch ex
+        if ex isa MethodError
+            less = true
+            for f in 1:fieldcount(T)
+                less &= shrinkless(getfield(a,f), getfield(b,f))
+            end
+            return less
+        else
+            rethrow(ex)
+        end
+    end
+end
+shrinkless(a::T, b::T) where {S, N, T <: AbstractArray{S,N}} = begin
+    return length(a) == length(b) ? reduce(&, map(shrinkless, a, b)) : length(a) < length(b)
+end
+
+shrink(f, case, singleArg, nShrinks=nShrinks[]) = begin
+    tests = 0
+    popSize = 100
+    bestCounterexample = Shrinker(case)
+    
+    population   = [ bestCounterexample for _ in 1:3*popSize ]
+    
+    while tests < nShrinks
+        population .= shrink.(population)
+        
+        survivors = if singleArg 
+            filter(x -> !f(x.case), population)
+        else
+            filter(x -> !f(x.case...), population)
+        end
+        
+        if isempty(survivors)
+            fill!(population, bestCounterexample)
+        else
+            bestN = partialsort!(survivors, 1:min(10, length(survivors)))
+            bestCounterexample = first(bestN)
+            population .= rand(bestN, 3*popSize)
+        end
+        tests += 1
+    end
+    bestCounterexample
+end
+
+shrink(s::T) where {S, T <: Shrinker{S}} = T(shrink(s.case), s.generation+1)
+shrink(s::T) where {S <: Tuple, T <: Shrinker{S}} = begin
+    ncase = shrink(s.case)
+    Shrinker{typeof(ncase)}(ncase, s.generation+1)
+end
+
+"""
+    shrink(a::T) where T
+
+Fallback shrinking function for structs. Tries to shrink each field of the given type and calls the default constructor. If you wrote a custom [`generate`](@ref), you probably also have to customize this.
+"""
+shrink(a::T) where T = T([ shrink(getfield(a,f)) for f in 1:fieldcount(T) ]...) # fallback for structs
+
+"""
+    shrink(n::T) where { T <: Number}
+
+Shrinks numbers toward zero.
+"""
+shrink(up::T) where {T <: Number} = begin
+    iszero(up) && return up
+    # this assumes isbits!
+    pow2s = UInt[]
+    for i in 0:(sizeof(T)*8)
+        n = UInt(1) << i
+        if (up & n) != 0
+            push!(pow2s, n)
+        end
+    end
+    up & T(~rand(pow2s))
+end
+
+"""
+    shrink(arr::T) where {V, N, T <: AbstractArray{V,N}}
+
+Shrinks arrays by shrinking their elements as well as dropping random elements with probability [`PropCheck.dropChance`](@ref).
+"""
+shrink(arr::T) where {V, N, T <: AbstractArray{V,N}} = begin
+    length(arr) == 0 && return ret
+    
+    # shrink array by shrinking elements and maybe dropping a random element
+    ret = copy(arr)
+    if rand() < dropChance[]
+        deleteat!(ret, rand(eachindex(ret)))
+    end
+    shrink.(ret)
+end
+
+"""
+    shrink(a::T) where T <: Tuple
+
+Shrinks tuples by shrinking their elements.
+"""
+shrink(a::T) where T <: Tuple = shrink.(a)
 
 end # module
