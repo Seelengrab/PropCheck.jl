71f609ee179671114ea02d4c3dd7e15d72a8f40d
Sukera <Seelengrab@users.noreply.github.com>
2021-09-01 19:56:30 +0200

    Changed tests to do-notation, remove impossible property and bump version

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,26 +6,21 @@ using PropCheck: getSubtypes
 Tests the fallback shrinking for numbers to shrink towards zero.
 """
 function numsToZero(T)
-    check(Integrated(Generator(T)), x -> begin
+    # are there other properties of the shrinkers themselves we can test?
+    check(Integrated(Generator(T))) do x
         shrunks = shrink(x)
         if x == zero(T)
             isempty(shrunks)
         else
             !isempty(shrunks) && all( y -> zero(T) <= abs(y) < abs(x), shrunks)
         end
-    end)
-end
-
-function noSkipping(T)
-    gen = Generator(T)
-    target = generate(gen)
-    check(Integrated(gen), !=(target))
+    end
 end
 
 @testset "All Tests" begin
-    @testset "$f" for f in (numsToZero, noSkipping)
+    @testset "numsToZero" begin
         @testset "$T" for T in union(getSubtypes(Integer), getSubtypes(AbstractFloat), (Float64, Float32, Float16))
-            @test f(T)
+            @test numsToZero(T) broken=(T == BigInt || T <: AbstractFloat)
         end
     end
 end
