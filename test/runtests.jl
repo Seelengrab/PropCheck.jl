12d55145435ceb427328f20cb9bb438c9d05f5f6
Sukera <Seelengrab@users.noreply.github.com>
2021-09-07 17:57:53 +0200

    Add test for filtering of Integrated generators

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,13 @@
 using Test
 using PropCheck
-using PropCheck: getSubtypes
+using PropCheck: getSubtypes, numTests
 
 """
 Tests the fallback shrinking for numbers to shrink towards zero.
 """
 function numsToZero(T)
     # are there other properties of the shrinkers themselves we can test?
-    check(Integrated(Generator(T))) do x
+    check(igen(T)) do x
         shrunks = shrink(x)
         if x == zero(T)
             isempty(shrunks)
@@ -17,10 +17,30 @@ function numsToZero(T)
     end
 end
 
+"""
+Tests that when a given predicate holds for the parent, it also holds for its subtrees (or at least the first 100).
+"""
+function predicateHoldsForSubtrees(p, T)
+    g = filter(p, igen(T))
+    toCheck = (first(g) for _ in 1:numTests[])
+    all(toCheck) do x
+        all(p ∘ root, Iterators.take(subtrees(x), 100))
+    end
+end
+
+const numTypes = union(getSubtypes(Integer), getSubtypes(AbstractFloat), (Float64, Float32, Float16))
+
 @testset "All Tests" begin
     @testset "numsToZero" begin
-        @testset "$T" for T in union(getSubtypes(Integer), getSubtypes(AbstractFloat), (Float64, Float32, Float16))
+        @testset "$T" for T in numTypes
             @test numsToZero(T) broken=(T == BigInt || T <: AbstractFloat)
+        end
+    end
+    @testset "filter predicates hold for shrunk values" begin
+        for p in (iseven, isodd)
+            @testset "$p($T)" for T in (UInt8, UInt16, UInt32, UInt64)
+                @test predicateHoldsForSubtrees(p, T) broken=(T == BigInt || T <: AbstractFloat)
+            end
         end
     end
 end
