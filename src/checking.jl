7292bded115ba0c9a7d73e42d21bc38f014c6da0
Sukera <Seelengrab@users.noreply.github.com>
2021-10-20 10:20:28 +0200

    Prettier output printing, explicit printing of last counterexample

--- src/checking.jl
+++ src/checking.jl
@@ -2,7 +2,8 @@ using Logging
 
 function check(p, i::Integrated, rng=Random.default_rng())
     genAs = [ generate(rng, freeze(i)) for _ in 1:numTests[] ]
-    something(findCounterexample(p, genAs), true)
+    res = findCounterexample(p, genAs)
+    return something(res, true)
 end
 
 minimize(f) = Base.Fix1(minimize, f)
@@ -16,6 +17,9 @@ function minimize(f, t::Tree{T}) where {T}
         !any(f, subs) && break
         t = first(Iterators.filter(f, subs))
     end
+    @info "$(length(shrinks)) counterexamples found"
+    minimum = last(shrinks)
+    @info "Smallest counterexample: " minimum
     return shrinks
 end
 
@@ -23,7 +27,7 @@ function findCounterexample(f, trees::Vector{<:Tree})
     _f = (!f ∘ root)
     filter!(_f, trees)
     isempty(trees) && return nothing
-    t = first(trees)
-    @info "Found counterexample for '$f', beginning shrinking..." t
-    minimize(_f, t)
+    example = first(trees).root
+    @info "Found counterexample for '$f', beginning shrinking..." example
+    minimize(_f, first(trees))
 end
