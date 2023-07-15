8272656ff2366531e002e9831754280ba45e994d
Sukera <Seelengrab@users.noreply.github.com>
2023-07-16 00:26:59 +0200

    Fix wrong argument checking in `@constfield` macro

--- src/util.jl
+++ src/util.jl
@@ -5,7 +5,7 @@ A macro providing compatibility for `const` fields in mutable structs.
 Gives a `const` `Expr` if supported, otherwise is a noop and just returns the field.
 """
 macro constfield(ex::Expr)
-    ex.head != :(::) || length(ex.args) != 2 || throw(ArgumentError("`@constfield` only supports expressions of the form `field::Type`!"))
+    (ex.head == Symbol("::") && length(ex.args) == 2) || throw(ArgumentError("`@constfield` only supports expressions of the form `field::Type`!"))
     ex = esc(ex)
     if VERSION < v"1.8"
         ex
